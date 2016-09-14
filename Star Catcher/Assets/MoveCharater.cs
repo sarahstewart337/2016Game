8ebb0bc337b7e373b2d35376fa74b4cd6bde2c2c
Unknown <sstewartyankeefan@yahoo.com>
2016-09-14 13:57:57 -0600

    Character Slide

--- Star Catcher/Assets/MoveCharater.cs
+++ Star Catcher/Assets/MoveCharater.cs
@@ -13,9 +13,37 @@ public class MoveCharater : MonoBehaviour
     public float gravity = 1;
     public float jumpSpeed = 1;
     public int jumpCount = 0;
-    public int jumpCountMax;
-	// Use this for initialization
-	void Start ()
+    public int jumpCountMax = 2;
+    //Sliding vars
+    public int slideDuration = 100;
+    public float slideTime = 0.01f;
+    //Coroutine for Sliding Character
+    IEnumerator Slide ()
+    {
+        //set a temp var to the value of slideDuration
+        int durationTemp = slideDuration;
+        //
+        float newSpeedTemp = Speed;
+        Speed +=Speed;
+        //while loops will run until true and then stop/ While loop runs while the slideDuration is greater than 0
+        while (slideDuration > 0)
+        {
+            //subtracts 1 from the slide duration
+            slideDuration--;
+            //yield "holds the coroutine"
+            //return sends to the coroutine to do an operation while yielding
+            // new creates an instance of an object
+            //waitforseconds is an object that waits for a duration of time
+            yield return new WaitForSeconds(slideTime);
+            
+        }
+        slideDuration = durationTemp;
+    }
+
+
+   
+    // Use this for initialization
+    void Start ()
     {
         //This "Finds" the charaacter controller component
         myCC = GetComponent<CharacterController>();
@@ -33,8 +61,20 @@ public class MoveCharater : MonoBehaviour
             //adding the jumpSpeed var to the tempPos var
             tempPos.y = jumpSpeed;
         }
+
+        //Start Sliding
+        if(Input.GetKey(KeyCode.RightArrow)&& Input.GetKeyDown(KeyCode.S))
+        {
+            //StartCoroutine is a function that calls coroutine. Use the coroutine in the argument
+            StartCoroutine(Slide());
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKeyDown(KeyCode.S))
+        {
+            //StartCoroutine is a function that calls coroutine. Use the coroutine in the argument
+            StartCoroutine(Slide());
+        }
         // test if character controller is grounded
-       if(myCC.isGrounded)
+        if (myCC.isGrounded)
         {
             // reset jump count if grounded
             jumpCount = 0;
