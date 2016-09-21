bc1330fbb4cf8ebe02b2fa2af1f71964b719e02c
Unknown <sstewartyankeefan@yahoo.com>
2016-09-21 13:55:35 -0600

    Recycle Level objects

---
+++ Star Catcher/Assets/scripts/recyclelevel/RecycleLevel.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class RecycleLevel : MonoBehaviour
+{
+
+    private Vector3 newLocation;
+    public List<Recycler> recyclableList;
+    private int i = 0;
+
+    //subscribe to the list action call
+    void Start ()
+    {
+        recyclableList = new List<Recycler>();
+        Recycler.RecycleAction += RecycleActionHandler;
+    }
+
+    private void RecycleActionHandler(Recycler _r)
+    {
+        recyclableList.Add(_r);
+    }
+
+    void OnTriggerEnter()
+    {
+        newLocation.x = StaticVars.nextSectionPosition;
+        recyclableList[i].cube.position = newLocation;
+        StaticVars.nextSectionPosition += StaticVars.distance;
+        if (i < recyclableList.Count-1)
+            i++;
+
+    }
+}
---
+++ Star Catcher/Assets/scripts/recyclelevel/StaticVars.cs
@@ -0,0 +1,11 @@
+﻿
+
+    public class StaticVars
+    {
+
+        public static float distance = 4;
+        public static float nextSectionPosition = 12;
+
+
+    }
+
