6d2d68ae178015f64a0c8dca0892019a36fb83ca
Sarah Stewart <sstewartyankeefan@yahoo.com>
2016-09-22 23:35:34 -0600

    sprint1

---
+++ Star Catcher/Assets/scripts/recyclelevel/Temppos.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Temppos : MonoBehaviour 
+
+	{
+
+
+    private Vector3 newLocation;
+
+
+
+    void OnTriggerEnter()
+    {
+        StaticVars.nextSectionPosition += StaticVars.distance;
+        newLocation.x = StaticVars.nextSectionPosition;
+        transform.position = newLocation;
+    }
+
+}
