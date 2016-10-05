e8d786a15501814eb178b86904942b6728bd84eb
Unknown <sstewartyankeefan@yahoo.com>
2016-10-05 13:55:54 -0600

    flip character

---
+++ Star Catcher/Assets/scripts/flip character/UserInputs.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public static Action <KeyCode> UserInput;
+
+public class UserInputs : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+       
+
+      if (Input.GetKeyDown(KeyCode.RightArrow)&& UserInput != null)
+        {
+            UserInput(KeyCode.RightArrow);
+
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && UserInput != null)
+        {
+            UserInput(KeyCode.LeftArrow);
+
+        }
+
+    }
+}
