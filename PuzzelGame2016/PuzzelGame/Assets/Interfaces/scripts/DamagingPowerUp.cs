dbf5c7ca923d709ca4b878c56266f3d16bcd051b
Unknown <sstewartyankeefan@yahoo.com>
2016-11-16 13:55:30 -0700

    inheritence 2

--- PuzzelGame2016/PuzzelGame/Assets/Interfaces/scripts/DamagingPowerUp.cs
+++ PuzzelGame2016/PuzzelGame/Assets/Interfaces/scripts/DamagingPowerUp.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System;
 
-public class DamagingPowerUp : MonoBehaviour, IDamage {
+public class DamagingPowerUp : MonoBehaviour, IDamage { 
     public void Damage()
     {
         
