2489c934d1afb5de49db954f69c7785db9708451
Unknown <sstewartyankeefan@yahoo.com>
2016-11-18 13:41:17 -0700

    overload

---
+++ PuzzelGame2016/PuzzelGame/Assets/Interfaces/scripts/Overloading.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Overloading : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Move (int speed)
+    {
+        Vector3 vector = new Vector3(speed, 0, 0);
+	
+	}
+}
