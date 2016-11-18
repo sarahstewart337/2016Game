c5b5dd101c4a0982d1f5b77fe07127f2ffbc3f35
Unknown <sstewartyankeefan@yahoo.com>
2016-11-18 13:57:38 -0700

    inheritence 2

---
+++ PuzzelGame2016/PuzzelGame/Assets/Interfaces/scripts/Animal.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Animal : MonoBehaviour {
+
+	// Use this for initialization
+	public virtual void Start ()
+    {
+        Walk();
+	
+	}
+	
+	// Update is called once per frame
+	void Walk ()
+    {
+        print("walk");
+	
+	}
+}
---
+++ PuzzelGame2016/PuzzelGame/Assets/Interfaces/scripts/Dog.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dog : Animal
+{
+    public override void Start()
+    {
+        base.Start();
+        Bark();
+    }
+    void Bark()
+    {
+        print("bark");
+    }
+
+	
+}
