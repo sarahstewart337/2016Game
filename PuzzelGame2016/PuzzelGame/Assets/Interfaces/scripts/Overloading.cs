4874ce806bbcb39608061b5cbefedaae9dbc3242
Unknown <sstewartyankeefan@yahoo.com>
2016-11-18 13:50:48 -0700

    overloading2

--- PuzzelGame2016/PuzzelGame/Assets/Interfaces/scripts/Overloading.cs
+++ PuzzelGame2016/PuzzelGame/Assets/Interfaces/scripts/Overloading.cs
@@ -4,7 +4,9 @@ using System.Collections;
 public class Overloading : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
+	void Update ()
+    {
+        Move(50);
 	
 	}
 	
@@ -12,6 +14,20 @@ public class Overloading : MonoBehaviour {
 	void Move (int speed)
     {
         Vector3 vector = new Vector3(speed, 0, 0);
-	
-	}
+        transform.position = vector*Time.deltaTime;
+    }
+    void Move (string speed)
+    {
+        Vector3 vector = Vector3.zero;
+        switch(speed)
+        {
+            case "Fast":
+                 vector = new Vector3(100, 0, 0);
+                break;
+            case "Slow":
+                vector = new Vector3(1, 0, 0);
+                break;
+        }
+        transform.position += vector*Time.deltaTime;
+    }
 }
