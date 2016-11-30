c498c58a6b1a66422a24be9acd04d6bea3ffc097
Unknown <sstewartyankeefan@yahoo.com>
2016-11-30 14:13:19 -0700

    delegates

---
+++ PuzzelGame2016/PuzzelGame/Assets/InClassHelp/Interfaces/EvenHandlerIntro.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EvenHandlerIntro : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	 {
+		 Delegatesintro.MyEvent += MyEventHandler;
+		 Delegatesintro.EventWithArgs += EventWithArgsHandler;
+		 Delegatesintro.EventString += EventStringHandler;
+		 Delegatesintro.EvtReturnArgs += EvtReturnArgsHandler;
+	
+	}
+	private string EvtReturnArgsHandler(string s)
+	{
+		return " The animals I Hate Most Are " + s;
+	}
+	private  string EventStringHandler()
+	{
+		print("Do You Like Cats?");
+		return "I Hate Cats";
+	}
+	
+	private void MyEventHandler()
+	{
+		print("I Ran");
+	}
+	private void EventWithArgsHandler(string s)
+	{
+		print(s);
+	}
+}
