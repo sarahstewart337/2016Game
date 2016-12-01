d9a31a048dc187bd41d37b003504c565027a89dd
Unknown <sstewartyankeefan@yahoo.com>
2016-12-01 13:58:59 -0700

    star catcher added

--- Sprint1/Star Catcher/Assets/scripts/Game/Stars/destroyStar.cs
+++ Sprint1/Star Catcher/Assets/scripts/Game/Stars/destroyStar.cs
@@ -5,47 +5,46 @@ public class destroyStar : MonoBehaviour
  {
     public int forceTime = 10;
     public float forceDuration = 0.1f;
-  
+    private bool canAddForce = true;
     private Rigidbody rigid;
     private float force;
+    public float forceRange = 10;
     private Vector3 forceVector;
     private Vector3 torqueVector;
-    public float forceRange = 10;
-    IEnumerator RunRandomForce ()
+
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+        StartCoroutine(RunRandomForce());
+    }
+
+    IEnumerator RunRandomForce()
     {
         force = Random.Range(-forceRange, forceRange);
-        while(forceTime > 0)
+        while (forceTime > 0)
         {
             yield return new WaitForSeconds(forceDuration);
             forceVector.x = force;
-            torqueVector.z -= force/force;
+            torqueVector.z = force / force;
             rigid.AddTorque(torqueVector);
             rigid.AddForce(forceVector);
             forceTime--;
         }
     }
 
-
-   void Start ()
-    {
-        rigid = GetComponent<Rigidbody>();
-        StartCoroutine(RunRandomForce());
-    }
-
     public float endTime = 3;
 
-  
-	
-	void onCollisionEnter()
+    void OnCollisionEnter()
     {
-        
+        canAddForce = false;
         Destroy(gameObject, endTime);
     }
 
-    /* IEnumerator EndStar()
-   {
-       yield return new WaitForSeconds(endTime);
+    IEnumerator EndStar ()
+    {
+        yield return new WaitForSeconds(endTime);
 
-   }
-   */
+    }
+    
 }
---
+++ Sprint1/Star Catcher/Assets/scripts/Game/enemyai/EnemyMove.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMove : MonoBehaviour
+{
+
+    public float speed;
+    public float gravity;
+    public float jumpstrength;
+    private Vector3 movement;
+    private CharacterController myCC;
+    private Vector3 tempPos;
+    public bool WolfRunning = true;
+    public Transform charArt;
+    private bool forward = true;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("running");
+        myCC = GetComponent<CharacterController>();
+        movement = new Vector3(speed, 0, 0);
+        //StartCoroutine(MoveHandler());
+
+    }
+
+    public void jumpHandler()
+    {
+        movement.y = jumpstrength;
+    }
+
+
+
+
+    public IEnumerator MoveHandler()
+    {
+
+        while (WolfRunning)
+        {
+            yield return new WaitForSeconds(.02f);
+
+
+
+            movement.y -= gravity * Time.deltaTime;
+            myCC.Move(movement * Time.deltaTime);
+
+            /*if (movement.y > (gravity * 5)) 
+            {
+                movement.y = gravity * 5;
+            }*/
+
+            if (transform.position.z != 0)
+            {
+                tempPos = transform.position;
+                tempPos.z = 0;
+                transform.position = tempPos;
+            }
+        }
+    }
+
+    public void SetDirection (bool runRight)
+    {
+        if(runRight)
+        {
+            movement = new Vector3(speed, 0, 0);
+            if (!forward)
+            {
+                charArt.Rotate(0, 180, 0);
+                forward = true;
+
+            }
+        }
+
+        else
+        {
+            movement = new Vector3(-speed, 0, 0);
+            if (forward)
+            {
+                charArt.Rotate(0, 180, 0);
+                forward = false;
+            }
+        }
+    }
+
+
+}
