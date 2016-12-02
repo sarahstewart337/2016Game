78ba964b5e50a18140436e12e9dda6acfc5edebb
Unknown <sstewartyankeefan@yahoo.com>
2016-12-01 19:06:23 -0700

    dunno

---
+++ Sprint1/Star Catcher/Assets/scripts/Game/Menu/EndGame.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class EndGame : MonoBehaviour
+{
+    public Button menuText;
+
+    // Use this for initialization
+    void Start()
+    {
+        menuText = menuText.GetComponent<Button>();
+
+    }
+
+    public void StartLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
+
+
