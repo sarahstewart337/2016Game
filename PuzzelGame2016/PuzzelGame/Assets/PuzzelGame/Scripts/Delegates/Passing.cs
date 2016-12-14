01b136d2d72d8a347adcf612932c11a1c96ad2f4
Unknown <sstewartyankeefan@yahoo.com>
2016-12-14 05:41:35 -0700

    delegates

---
+++ PuzzelGame2016/PuzzelGame/Assets/PuzzelGame/Scripts/Delegates/Passing.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Passing : MonoBehaviour
+{
+    public int speed = 3;
+    public static Action<int> Speed;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Speed += SpeedHandler;
+	}
+	
+	public void SpeedHandler(int speed)
+    {
+        print("the character is running " + speed + "mph");
+
+    }
+}
--- PuzzelGame2016/PuzzelGame/Assets/PuzzelGame/Scripts/inheritance/Move Blocks.cs
+++ PuzzelGame2016/PuzzelGame/Assets/PuzzelGame/Scripts/inheritance/Move Blocks.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Fly : MonoBehaviour {
+public class MoveBlocks : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
