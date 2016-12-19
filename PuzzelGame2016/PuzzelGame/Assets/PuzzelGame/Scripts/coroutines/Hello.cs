1169ba63889543e74e2c1b5b3f1ce085de30c53e
Unknown <sstewartyankeefan@yahoo.com>
2016-12-19 16:59:50 -0700

    practice

---
+++ PuzzelGame2016/PuzzelGame/Assets/PuzzelGame/Scripts/coroutines/Hello.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hello : MonoBehaviour {
+
+	
+	void Start ()
+    {
+        StartCoroutine(Hey());
+	}
+	
+    IEnumerator Hey()
+    {
+        yield return new WaitForSeconds(3);
+        print("Hello Welcome to this awesom game!");
+    }
+
+	
+}
