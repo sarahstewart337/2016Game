500a25cfd63e470a9f03618a2ee5e03710a93f6b
Unknown <sstewartyankeefan@yahoo.com>
2016-12-19 17:09:14 -0700

    practice

---
+++ PuzzelGame2016/PuzzelGame/Assets/PuzzelGame/Scripts/coroutines/TurnLightsOn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnLightsOn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        StartCoroutine(Lights());
+		
+	}
+	
+	IEnumerator Lights()
+    {
+        yield return new WaitForSeconds(2);
+        print("I turned the lights on");
+    }
+}
